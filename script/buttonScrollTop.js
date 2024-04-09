9d580e5c7397b6bd6b8fdc576acc12ac8c6cecb2
Leonardo3737 <leovinicius3737@gmail.com>
2024-04-08 21:46:58 -0400

    adicionar slide

--- script/buttonScrollTop.js
+++ script/buttonScrollTop.js
@@ -1,8 +1,6 @@
-let tecnologias
-
-window.onscroll = function() {
-  exibirOcultarBotao();
-};
+let slider
+let sliderPositions
+let aux = 0
 
 function exibirOcultarBotao() {
   var arrow = document.getElementById("arrow");
@@ -26,19 +24,41 @@ function voltarAoTopo() {
   }
 }
 
-document.addEventListener("DOMContentLoaded", () => {
-  tecnologias = [
-    document.getElementById('typescript'),
-    document.getElementById('react'),
-    document.getElementById('node'),
-    document.getElementById('mysql')
-  ]
-  tecnologias.map((tec)=> {
-    tec.style.display = 'none'
-  })
-});
+function controlShowButton() {
+  if(aux === 1){
+    document.getElementById('buttonSliderR').style.display = 'block'
+    document.getElementById('buttonSliderL').style.display = 'block'
+    document.getElementById('buttons').style.justifyContent = 'space-between'
+  }
+  if(aux === 0) {
+    document.getElementById('buttonSliderL').style.display = 'none'
+    document.getElementById('buttons').style.justifyContent = 'end'
+  }
+  if(aux === 2) {
+    document.getElementById('buttonSliderR').style.display = 'none'
+    document.getElementById('buttons').style.justifyContent = 'start'
+  }
+}
 
-function showTec(tec) {
+function renderSlide() {
+  controlShowButton()
+  const larguraTela = window.innerWidth;
+  sliderPositions = larguraTela > 800 ? ['-105%', '0%', '105%'] : ['-105%', '0%', '112.5%']
+  slider.style.right = sliderPositions[aux]
+}
+
+function movSlider(direction) {
+  if (direction === 'right') aux++
+  if (direction === 'left') aux--
+  controlShowButton()
+  slider.style.transition = '0.75s'
+  slider.style.right = sliderPositions[aux]
+}
+
+
+function headerControl(tec, classN) {
+  document.getElementById('description').style.display = 'none'
+  const tecnologias = Array.from(document.getElementsByClassName(classN))
   tecnologias.map((m, i)=> {
     if(i === tec) {
       m.style.display = 'block'
@@ -48,3 +68,26 @@ function showTec(tec) {
   })
 }
 
+document.addEventListener("DOMContentLoaded", () => {
+  console.log(document.getElementsByClassName('test')[0]);
+  document.getElementById('description').style.display = 'block'
+  document.getElementById('Slider').classList.add('slider')
+
+  const displayNone = Array.from(document.getElementsByClassName('displayNoneJS'))
+  displayNone.map((tec)=> {
+    tec.style.display = 'none'
+  })
+
+  slider = document.getElementById('Slider')
+  renderSlide()
+  controlShowButton()
+});
+
+window.addEventListener('resize', function() {
+  renderSlide()
+  controlShowButton()
+});
+
+window.onscroll = function() {
+  exibirOcultarBotao();
+};
